6fb2af6157d747ce4505ffda67cfb7ff08a1286d
Thais Cristine <this.cristine06@gmail.com>
2020-10-18 08:59:30 +0300

    adding error handler

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,23 +1,28 @@
 import { Request, Response } from 'express';
-import { getRepository } from 'typeorm'
-import Orphanage from '../models/Orphanage'
+import { getRepository } from 'typeorm';
+import Orphanage from '../models/Orphanage';
+import orphanageView from '../views/orphanages_view';
 
 export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
 
-        return response.json(orphanages);
+        return response.json(orphanageView.renderMany(orphanages));
     },
     async show(request: Request, response: Response) {
 
         const { id } = request.params;
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orphanage);
+        return response.json(orphanageView.render(orphanage));
     },
     async create(request: Request, response: Response) {
         const {
@@ -36,8 +41,8 @@ export default {
         const images = requestImages.map((image) => {
             return { path: image.filename}
         })
-    
-        const orphanage = orphanagesRepository.create({
+
+        const data = {
             name,
             latitude,
             longitude,
@@ -46,7 +51,9 @@ export default {
             opening_hours,
             open_on_weekends,
             images
-        });
+        }
+    
+        const orphanage = orphanagesRepository.create(data);
     
         await orphanagesRepository.save(orphanage);
     
